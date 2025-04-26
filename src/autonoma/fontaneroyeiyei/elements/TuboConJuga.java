fb7993c26470ce695d1dc345cb48f2d20d6a82cb
mateo <mateoq152006@gmail.com>
2025-04-26 09:38:40 -0500

    Se crea los atributos y metodos lac lase hija de tuboConfuga

--- src/autonoma/fontaneroyeiyei/elements/TuboConJuga.java
+++ src/autonoma/fontaneroyeiyei/elements/TuboConJuga.java
@@ -18,7 +18,7 @@ public class TuboConJuga extends Tubo implements Reparable{
 
     
     @Override
-    public void reparar() {
+    public void repara() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
