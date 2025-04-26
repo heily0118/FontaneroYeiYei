0f229c2d519d93543bed5574d1f845b549daebe1
mateo <mateoq152006@gmail.com>
2025-04-26 09:04:30 -0500

    Se crea los atributos y metodos de acceso de clase fuga

--- src/autonoma/fontaneroyeiyei/elements/Fuga.java
+++ src/autonoma/fontaneroyeiyei/elements/Fuga.java
@@ -6,8 +6,87 @@ package autonoma.fontaneroyeiyei.elements;
 
 /**
  *
- * @author USUARIO
+ * @author Mateo Quintero Morales <mateo.quinterom@autonoma.edu.co>
+ * @since 20250425
+ * @version 1.0.0
  */
 public class Fuga {
+    
+    ////////////////////////////////
+    /// Atributos
+    
+    private int posX;
+    private int posy;
+    private boolean activa;
+
+    
+    
+    /////////////////////////////////
+    /// Constructor
+    ////
+    public Fuga(int posX, int posy, boolean activa) {
+        this.posX = posX;
+        this.posy = posy;
+        this.activa = activa;
+    }
+
+    
+    //////////////////////////////////
+    /// Metodos de acceso (get)
+    ///
+    public int getPosX() {
+        return posX;
+    }
+    
+    public boolean isActiva() {
+        return activa;
+    }
+    
+    public int getPosy() {
+        return posy;
+    }
+
+    
+    //////////////////////////////////
+    /// Metodos de acceso (set)
+    ///
+    
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+    
+    public void setPosy(int posy) {
+        this.posy = posy;
+    }
+
+    
+
+    public void setActiva(boolean activa) {
+        this.activa = activa;
+    }
+    
+    
+    
+    //////////////////////////////////
+    /// Metodos
+    
+    public void activarFuga(){
+    
+      this.activa = true;
+    }
+    
+    
+    public void desactivarFuga(){
+    
+      this.activa = false;
+    }
+    
+    public boolean esActiva() {
+        return activa;
+    }
+
+    
+    
+    
     
 }
