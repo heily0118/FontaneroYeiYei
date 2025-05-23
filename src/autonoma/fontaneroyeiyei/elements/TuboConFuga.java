30a24e98f3ab66413ca552061f14626acdcb293e
Heily Rios <heilyayala005@gmail.com>
2025-05-22 20:19:09 -0500

    Se crearon nuevas clases

---
+++ src/autonoma/fontaneroyeiyei/elements/TuboConFuga.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.fontaneroyeiyei.elements;
+
+import java.awt.Graphics;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author Mateo Quintero Morales <mateo.quinterom@autonoma.edu.co>
+ * @since 20250425
+ * @version 1.0.0
+ */
+public class TuboConFuga extends Tubo {
+    
+    private Fuga fuga;
+
+    public TuboConFuga(String estado, int x, int y, int width, int height, Fuga fuga) {
+        super(estado, x, y, width, height);
+        this.fuga = fuga;
+        this.setImage(new ImageIcon("/autonoma/fontaneroyeiyei/images/TuboMalo.png")); 
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        if (this.isVisible() && this.getImage() != null) {
+            g.drawImage(this.getImage().getImage(), this.getX(), this.getY(), this.getWidth(), this.getHeight(), null);
+        }
+    }
+
+    public Fuga getFuga() {
+        return fuga;
+    }
+    
+}
